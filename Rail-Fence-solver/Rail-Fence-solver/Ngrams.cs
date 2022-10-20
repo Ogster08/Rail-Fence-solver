f4c3f82aeeb76ee83d10e22950f3d38eb8ecc8a6
Ogster08 <104634399+Ogster08@users.noreply.github.com>
2022-10-20 22:11:24 +0100

    first build

---
+++ Rail-Fence-solver/Rail-Fence-solver/Ngrams.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks.Sources;
+
+namespace Rail_Fence_solver
+{
+    internal class Ngrams
+    {
+        private Dictionary<string, double> ngrams_;
+        private int l_;
+        private double sum_;
+        private double floor_;
+
+        public Ngrams(string filename)
+        {
+            ngrams_ = new Dictionary<string, double>();
+            string[] lines = File.ReadAllLines(filename);
+
+            foreach (string item in lines)
+            {
+                string[] keyPair = item.Split(" ");
+                double value = double.Parse(keyPair[1]);
+                string key = keyPair[0];
+
+                ngrams_[key] = value;
+            }
+
+            l_ = lines[0].Split(" ")[0].Length;
+            sum_ = ngrams_.Values.ToArray().Sum();
+
+            foreach (string key in ngrams_.Keys.ToArray())
+            {
+                ngrams_[key] = Math.Log10(ngrams_[key] / sum_);
+            }
+
+            floor_ = Math.Log10(0.01 / sum_);
+        }
+
+        public double score(string text)
+        {
+            double score = 0;
+            for (int i = 0; i < text.Length - l_ + 1; i++)
+            {
+                if (ngrams_.ContainsKey(text.Substring(i, l_).ToUpper())) { score += ngrams_[text.Substring(i, l_).ToUpper()]; }
+                else { score += floor_; }
+            }
+
+            return score;
+        }
+    }
+}
--- Rail-Fence-solver/Rail-Fence-solver/Program.cs
+++ Rail-Fence-solver/Rail-Fence-solver/Program.cs
@@ -1,14 +1,15 @@
 ﻿namespace Rail_Fence_Solver
 {
+    using Rail_Fence_solver;
     using System;
     using System.Collections.Generic;
-    using System.Collections.Concurrent;
-    using System.ComponentModel;
+    using System.Xml.Schema;
 
     class Program
     {
         static void Main(string[] args)
         {
+
             while (true) 
             { 
                 string text = "";
@@ -19,6 +20,7 @@
                     Console.WriteLine();
                 }
 
+                List<Tuple<string, int>> possibleDecryptions = new();
                 for (int keyLength = 2; keyLength <= 15; keyLength++)
                 {
                     char[][] railFence = new char[keyLength][];
@@ -56,14 +58,26 @@
                         possibleDecryption[i] = railFence[indexes[0]][indexes[1]];
                     }
 
-                    Console.WriteLine($"{string.Join("", possibleDecryption)} {keyLength}");
-                    Console.WriteLine();
-
+                    Tuple<string, int> toAdd = new Tuple<string, int>(string.Join("", possibleDecryption), keyLength);
+                    possibleDecryptions.Add(toAdd);
                 }
+
+                Ngrams ngrams = new("english_quadgrams.txt");
+                double[] scores = new double[possibleDecryptions.Count];
+
+                for (int i = 0; i < scores.Length; i++)
+                {
+                    scores[i] = ngrams.score(string.Join("", possibleDecryptions[i].Item1.Where(char.IsLetter).ToArray()));
+                }
+
+                Tuple<string, int> decryption = possibleDecryptions[Array.IndexOf(scores, scores.Max())];
+                Console.WriteLine($"{decryption.Item1} keylength: {decryption.Item2}");
+                Console.WriteLine();
+
             }
         }
 
-        static Tuple<int[],bool> NextRailFence(int[] currentRailFence, int keyLength, bool countUp)
+        public static Tuple<int[],bool> NextRailFence(int[] currentRailFence, int keyLength, bool countUp)
         {
             int[] railFence = new int[2];
             if (countUp & (currentRailFence[0] + 1 == keyLength)) { countUp = false; }
@@ -72,30 +86,6 @@
             railFence[1] = currentRailFence[1] + 1;
             
             return Tuple.Create(railFence, countUp);
-        }
-
-        public static Dictionary<string, int> TextFrequency(string testText)
-        {
-            var characterCount = new Dictionary<string, int>() { { "a", 0 }, { "b", 0 }, { "c", 0 }, { "d", 0 }, { "e", 0 }, { "f", 0 }, { "g", 0 }, { "h", 0 }, { "i", 0 }, { "j", 0 }, { "k", 0 }, { "l", 0 }, { "m", 0 }, { "n", 0 }, { "o", 0 }, { "p", 0 }, { "q", 0 }, { "r", 0 }, { "s", 0 }, { "t", 0 }, { "u", 0 }, { "v", 0 }, { "w", 0 }, { "x", 0 }, { "y", 0 }, { "z", 0 } };
-
-            foreach (char c in testText) { characterCount[c.ToString()]++; }
-            return characterCount;
-        }
-
-        public static double ChiSquareTest(string testText)
-        {
-            var exspectedFrequencies = new Dictionary<string, double>() { { "e", 11.1607 }, { "a", 8.4966 }, { "r", 7.5809 }, { "i", 7.5448 }, { "o", 7.1635 }, { "t", 6.9509 }, { "n", 6.6544 }, { "s", 5.7351 }, { "l", 5.4893 }, { "c", 4.5388 }, { "u", 3.6308 }, { "d", 3.3844 }, { "p", 3.1671 }, { "m", 3.0129 }, { "h", 3.0034 }, { "g", 2.4705 }, { "b", 2.0720 }, { "f", 1.8121 }, { "y", 1.7779 }, { "w", 1.2899 }, { "k", 1.1016 }, { "v", 1.0074 }, { "x", 0.2902 }, { "z", 0.2722 }, { "j", 0.1965 }, { "q", 0.1962 } };
-            Dictionary<string, int> textFrequencies = TextFrequency(testText);
-
-            double score = 0;
-
-            foreach (string d in textFrequencies.Keys)
-            {
-                string s = d.ToLower();
-                double exspectedCount = exspectedFrequencies[s] / 100 * testText.Length;
-                score += Math.Pow(textFrequencies[s] - exspectedCount, 2) / exspectedCount;
-            }
-            return score;
         }
     }
 }
